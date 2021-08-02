8c7044e548e181e7e0a5e66b197718f108b73fcc
StanislavNikitin <snikitin@provectus.com>
2021-08-02 16:13:29 +0300

    upd lambdas 2

--- lambdas/incoming-msg-handler/app.py
+++ lambdas/incoming-msg-handler/app.py
@@ -3,14 +3,14 @@ import os
 
 import boto3
 
-MAX_BATCH_SIZE = 10
-SQS_QUEUE_NAME = os.getenv('SQS_QUEUE_NAME')
 sqs_resource = boto3.resource('sqs')
 
+MAX_BATCH_SIZE = 10
 
-def write_batch_sqs(messages: list):
-    queue = sqs_resource.get_queue_by_name(QueueName=SQS_QUEUE_NAME)
+
+def write_batch_sqs(messages: list, queue):
     batches = [messages[x:x + MAX_BATCH_SIZE] for x in range(0, len(messages), MAX_BATCH_SIZE)]
+    print(f'NUM OF BATCHES: {len(batches)}')
 
     for batch in batches:
         entries = []
@@ -21,31 +21,13 @@ def write_batch_sqs(messages: list):
             })
 
         response = queue.send_messages(Entries=entries)
-        print(f'PUSH_TO_QUEUE RESPONSE: {response}')
+        print(f'PUSH-TO-QUEUE RESPONSE: {response}')
 
 
 def lambda_handler(event, context):
     print(event['Records'])
-    """
-    [{'EventSource': 'aws:sns', 
-    'EventVersion': '1.0', 
-    'EventSubscriptionArn': 'arn:aws:sns:ca-central-1:630063752049:test-incoming-msg-topic:60affe38-2824-4f9d-a9a6-c00c186debf5', 
-    'Sns': {'Type': 'Notification', 
-            'MessageId': '166f4ff5-7d3e-56fc-9a03-1681d53578f2', 
-            'TopicArn': 'arn:aws:sns:ca-central-1:630063752049:test-incoming-msg-topic', 
-            'Subject': None, 
-            
-            'Message': '{\n  "originationNumber":"+14255550182",\n  "destinationNumber":"+12125550101",\n  "messageKeyword":"JOIN",\n  "messageBody":"EXAMPLE",\n  "inboundMessageId":"cae173d2-66b9-564c-8309-21f858e9fb84",\n  "previousPublishedMessageId":"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"\n}', 
-            
-            'Timestamp': '2021-07-23T11:19:39.334Z', 
-            'SignatureVersion': '1', 
-            'Signature': 'O+lemvM4TsYUQv+anEkYVt7ag82YwdAMudrSX8bq+MbSSuPeQp+aTSTCR0g3iUDCjwMtiLkUfP24hQxKzMNZD/YcP/Z9lXRGLrTdj2R9w70Le9AW9FOnQlPQ60VdcipOaSuzReU4SsW5prvhT4Jao2qctuSN6pHOILMSrVdmRAJdtwS/LDLVAZdMQFye3mSvLtPBzpz5z55XeCb8JfCoBvEsaZkflw2MIYE2FZ4gM+n+qXxKzWe4vmFzzBsrUJTQgdCUjxnnggqYQDzDTXhiVnqQ72WXRjJq9AwKTVwitPNbnG60AZIsmTfuT+703nTQ/ysVaT5o3jwQCPCc4sIYIA==', 
-            'SigningCertUrl': 'https://sns.ca-central-1.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem', 
-            'UnsubscribeUrl': 'https://sns.ca-central-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:ca-central-1:630063752049:test-incoming-msg-topic:60affe38-2824-4f9d-a9a6-c00c186debf5', 
-            'MessageAttributes': {}
-            }
-    }]
-    """
+
+    sqs_queue_name = os.getenv('SQS_QUEUE_NAME')
     messages = []
 
     for record in event['Records']:
@@ -67,4 +49,6 @@ def lambda_handler(event, context):
                 # some DynamoDB data
             })
 
-    write_batch_sqs(messages)
+    print(f'Downstream SQS queue: {sqs_queue_name}')
+    queue = sqs_resource.get_queue_by_name(QueueName=sqs_queue_name)
+    write_batch_sqs(messages, queue)
