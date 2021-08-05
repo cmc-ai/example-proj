62f469579f963cc75aea6170d2791307dd204aba
StanislavNikitin <snikitin@provectus.com>
2021-08-05 15:16:24 +0300

    pynamodb 5

--- lambdas/common/debt_record_model.py
+++ lambdas/common/debt_record_model.py
@@ -6,8 +6,8 @@ from pynamodb.attributes import UnicodeAttribute, NumberAttribute
 
 class DebtRecordModel(Model):
     class Meta:
-        table_name = os.getenv('DYNAMODB_TABLE')  # , 'chatbot-dev-sessions'
-        region = os.getenv('AWS_REGION')  # , 'ca-central-1'
+        table_name = os.getenv('DYNAMODB_TABLE', 'chatbot-dev-sessions')  #
+        region = os.getenv('AWS_REGION', 'ca-central-1')  #
 
     debt_id = NumberAttribute(hash_key=True)
     journey_id = NumberAttribute()
