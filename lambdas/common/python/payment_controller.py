4c0b0d9cfa395713c7f030df472bc50d18be8579
StanislavNikitin <snikitin@provectus.com>
2021-09-01 17:34:38 +0300

    range key query 1

--- lambdas/common/python/payment_controller.py
+++ lambdas/common/python/payment_controller.py
@@ -64,7 +64,7 @@ class DebtPaymentController:
 
         # get link and exp_utc_dt from Dynamo
         try:
-            payment_link_item = PaymentLinkModel.query(debt_id=self.debt_id)
+            payment_link_item = [p for p in PaymentLinkModel.query(debt_id=self.debt_id)][0]
             print(f'PaymentLink exists, {payment_link_item.attribute_value}')
             payment_link = payment_link_item.attribute_values.get('link')
             expiration_utc_unix_ts = int(payment_link_item.attribute_values.get('expiration_utc_ts'))
