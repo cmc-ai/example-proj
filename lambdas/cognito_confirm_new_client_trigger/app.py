233d75fa8e8b4a65e74fefe473b49592f9a84415
Vitaliy Krasnov <vkrasnov@provectus.com>
2021-09-30 12:39:11 +0300

    Updated cognito confirm new client trigger

--- lambdas/cognito_confirm_new_client_trigger/app.py
+++ lambdas/cognito_confirm_new_client_trigger/app.py
@@ -3,4 +3,10 @@ def lambda_handler(event, context):
 
     event['response']['autoConfirmUser'] = 'True'
 
+    if 'email' in event['request']['userAttributes']:
+        event['response']['autoVerifyEmail'] = True
+
+    if 'phone_number' in event['request']['userAttributes']:
+        event['response']['autoVerifyPhone'] = True
+
     return event
