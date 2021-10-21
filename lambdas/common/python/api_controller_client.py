7c5ea102424ef26ef34db00eaffc247a668ecf0f
Vitaliy Krasnov <vkrasnov@provectus.com>
2021-10-21 14:48:09 +0300

    Added portfolio id to reports

--- lambdas/common/python/api_controller_client.py
+++ lambdas/common/python/api_controller_client.py
@@ -457,43 +457,72 @@ class OtherAPIController(APIController):
             day=1, hour=0, minute=0, second=0).strftime("%Y-%m-%d %H:%M:%S")
         end_date = self.params.get('endDate') or datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
+        portfolio_id = self.params.get('portfolioId', None)
+        print(f"Selected portfolio id: {portfolio_id}")
 
-        query = f"""
+        print("FILTER STRING")
+        print({self._build_filter_string(limit_offset=False)})
+
+        if portfolio_id:
+            query = f"""
+                        SELECT status, clientportfolioid, COUNT(*) AS num, SUM(originalbalance) as originalbalance FROM debt
+                        WHERE createdate >= '{start_data}' and createdate <= '{end_date}' and clientportfolioid={portfolio_id}  
+                        GROUP BY status
+                    """
+        else:
+            query = f"""
                     SELECT status, COUNT(*) AS num, SUM(originalbalance) as originalbalance FROM debt
                     WHERE createdate >= '{start_data}' and createdate <= '{end_date}' 
-                    GROUP BY status;
-                """
+                    GROUP BY status
+                    """
+
         newly_added_items = self._map_cols_rows(*self._execute_select(query))
         print(f"newly_added_items: {newly_added_items}")
 
         query = f"""
                     SELECT COUNT(*) AS num FROM debt
-                    WHERE lastupdatedate >= '{start_data}' and lastupdatedate <= '{end_date}';
+                    WHERE lastupdatedate >= '{start_data}' and lastupdatedate <= '{end_date}'
                 """
+        if portfolio_id:
+            query += f" and clientportfolioid={portfolio_id}"
+
         completes_items = self._map_cols_rows(*self._execute_select(query))
         print(f"completes_items: {completes_items}")
         completes_count = completes_items[0]['num'] or 0 if completes_items else 0
 
         query = f"""
                     SELECT COUNT(*) AS num FROM debt
-                    WHERE lastupdatedate >= '{start_data}' and lastupdatedate <= '{end_date}' and status = 'inactive';
+                    WHERE lastupdatedate >= '{start_data}' and lastupdatedate <= '{end_date}' and status = 'inactive'
                 """
+        if portfolio_id:
+            query += f" and clientportfolioid={portfolio_id}"
+
         inactive_items = self._map_cols_rows(*self._execute_select(query))
         print(f"inactive_items: {inactive_items}")
         inactive_count = inactive_items[0]['num'] or 0 if inactive_items else 0
 
-        query = f"""
+        if portfolio_id:
+            query = f"""
+                    SELECT SUM(amount) AS amount FROM debtpayment AS dp INNER JOIN debt as d on dp.debtid=d.id
+                    WHERE dp.paymentDateTimeUTC >= '{start_data}' and dp.paymentDateTimeUTC <= '{end_date}' and
+                    d.clientportfolioid={portfolio_id}
+                    """
+        else:
+            query = f"""
                     SELECT SUM(amount) AS amount FROM debtpayment
                     WHERE paymentDateTimeUTC >= '{start_data}' and paymentDateTimeUTC <= '{end_date}';
-                """
+                    """
         collected_amounts = self._map_cols_rows(*self._execute_select(query))
         print(f"collected_amounts: {collected_amounts}")
         collected_amount = collected_amounts[0]['amount'] or 0 if collected_amounts else 0
 
         query = f"""
                     SELECT SUM(outstandingBalance) AS amount FROM debt
-                    WHERE lastupdatedate >= '{start_data}' and lastupdatedate <= '{end_date}';
+                    WHERE lastupdatedate >= '{start_data}' and lastupdatedate <= '{end_date}'
                 """
+        if portfolio_id:
+            query += f" and clientportfolioid={portfolio_id}"
+
         original_balances = self._map_cols_rows(*self._execute_select(query))
         print(f"original_balances: {original_balances}")
         original_balance = original_balances[0]['amount'] or 0 if original_balances else 0
